6514ecb92e32e68da1f22c01c81403a77d6a8db4
mochi-sann <moyurusuto@gmail.com>
2021-01-28 22:25:47 +0900

    わかんね

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +7,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Startup Name Generator'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final _suggestions = <WordPair>[];
+    final _saved = Set<WordPair>(); // NEW
+
+    final _biggerFont = TextStyle(fontSize: 28.0);
+    Widget _buildRow(WordPair pair) {
+      final alreadySaved = _saved.contains(pair);
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+
+        trailing: Icon(
+          // NEW from here...
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ), // ... to here.
+        onTap: () {
+          // NEW lines from here...
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        }, // ... to here.
+      );
+    }
+
+    Widget _buildSuggestions() {
+      return ListView.builder(
+          padding: EdgeInsets.all(1.0),
+          itemBuilder: /*1*/ (context, i) {
+            if (i.isOdd) return Divider(); /*2*/
+
+            final index = i ~/ 2; /*3*/
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+            }
+            return _buildRow(_suggestions[index]);
+          });
+    }
+
+    final wordPair = WordPair.random();
+    return _buildSuggestions();
   }
 }
